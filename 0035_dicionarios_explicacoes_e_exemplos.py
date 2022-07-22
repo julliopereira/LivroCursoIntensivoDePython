eb3de7c1b09d4de766d62ef6f8e1323820919995
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-22 15:11:01 -0300

    Auto Atualização para GitHub

--- 0035_dicionarios_explicacoes_e_exemplos.py
+++ 0035_dicionarios_explicacoes_e_exemplos.py
@@ -5,5 +5,9 @@ Dicionários são representados por colchetes { } e
 funciona um pouco parecido como Listas no entanto 
 cada valor tem duas instruções chamados chave e 
 valor.
-chave: valor
+{ chave: valor, chave: valor } 
 '''
+
+### EXEMPLO:
+
+alien_0 = {'cor': 'verde', }
