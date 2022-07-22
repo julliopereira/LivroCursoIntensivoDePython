0834ef033a9c4930b205dd69d4e09b9c29284eb8
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-22 00:37:01 -0300

    Auto Atualização para GitHub

--- 0034_if_e_else_exercicio5.py
+++ 0034_if_e_else_exercicio5.py
@@ -3,11 +3,12 @@
 usuarios = ['maria', 'paulo', 'silvia', 'joao', 'ciro', 'jose', 'admin']
 
 ### ESCREVER UMA SAUDAÇÃO PARA CADA USUÁRIO APÓS LOGIN
+
+
+### SE O USUÁRIO FOR admin 
+print('-'*50)
 login = 'joao'
 
 for usuario in usuarios:
     if login.lower() in usuario:
         print('\nOlá senhor "' + str(usuario).title() + '" você está entrando no sistema... \n' )
-
-### 
-print('-'*50)
