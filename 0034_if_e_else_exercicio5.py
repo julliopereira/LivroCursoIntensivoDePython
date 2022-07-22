e82d09efcfb217c60f4115419e057dc72f0886d9
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-22 03:00:01 -0300

    Auto Atualização para GitHub

--- 0034_if_e_else_exercicio5.py
+++ 0034_if_e_else_exercicio5.py
@@ -26,3 +26,24 @@ print('-'*50)
 current_users = ['maria', 'paulo', 'silvia', 'admin', 'joao', 'ciro', 'jose']
 new_users =  ['selma', 'samanta', 'sara', 'silvio', 'joao', 'paulo']
 
+### PERCORRER A LISTA new_users PARA VERIFICAR SE OS USUARIOS JÁ FORAM UTILIZADOS EM current_users
+
+for user in new_users:
+    if user.lower() in current_users:  # USUARIO John ou JOHN ou john SAO A MESMA COISA
+        print('Usuário "' + str(user) + '" existente, troque de usuário. \n')
+
+
+
+### CRIE UMA LISTA DE NUMEROS 
+print('-'*50)
+
+numeros = list(range(1, 9+1))
+
+### USE UM for PARA PERCORRER E if PARA MOSTRAR OS NUMEROS ORDINAIS
+for numero in numeros:
+    if numero == 1:
+        print( str(numero) + 'st')
+    elif numero == 2:
+        print( str(numero) + 'nd')
+    else:
+        print( str(numero) + 'th')
