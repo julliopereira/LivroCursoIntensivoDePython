185da2e42632057f22517d83621442b666374845
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-22 00:36:01 -0300

    Auto Atualização para GitHub

--- 0034_if_e_else_exercicio5.py
+++ 0034_if_e_else_exercicio5.py
@@ -1,13 +1,13 @@
 ### CRIAR UMA LISTA admin COM 5 USUARIOS
 
-admin = ['maria', 'paulo', 'silvia', 'joao', 'ciro', 'jose']
+usuarios = ['maria', 'paulo', 'silvia', 'joao', 'ciro', 'jose', 'admin']
 
 ### ESCREVER UMA SAUDAÇÃO PARA CADA USUÁRIO APÓS LOGIN
 login = 'joao'
 
-for a in admin:
-    if login.lower() in a:
-        print('\nOlá senhor "' + str(a).title() + '" você está entrando no sistema... \n' )
+for usuario in usuarios:
+    if login.lower() in usuario:
+        print('\nOlá senhor "' + str(usuario).title() + '" você está entrando no sistema... \n' )
 
 ### 
 print('-'*50)
