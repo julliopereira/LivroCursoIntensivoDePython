26f468bbda46c213911099f77de52f63bdfe2d23
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-22 00:12:01 -0300

    Auto Atualização para GitHub

--- 0033_test.py
+++ 0033_test.py
@@ -1,10 +1,11 @@
 ### VERIFICAR SE LISTA ESTÁ VAZIA
+### SE A 
 
 listas = []
 
 if listas:
     for lista in listas:
-        print('Adicionando' + lista + '.\n')
+        print('Adicionando ' + lista + '.\n')
     print('\nFinalizado, fazendo a pizza')
 else:
     print('Está certo de que quer uma pizza sem recheio ?')
