021f686b96fca153bac28023bb09c9a5725d4890
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-22 19:10:01 -0300

    Auto Atualização para GitHub

--- 0040_dicionarios_percorrendo_dic_com_laco.py
+++ 0040_dicionarios_percorrendo_dic_com_laco.py
@@ -16,6 +16,18 @@ for k, v in usuarios.items():               # ATRIBUI ÀS VARIÁVEIS k e v OS VA
     print(str(k) + ':' + str(v))            # MOSTRA O VALOR key e values
 print('\n')
 
-### 
+
+### PARA TRABALHAR COM O VALOR keys É NECESSÁRIO UTILIZAR O MÉTODO keys()
 print('-'*50)
+
+num_favoritos = { 
+    'joao': '30',
+    'sara': '10',
+    'eduardo': '20',
+    'phil': '40',
+    }
+
+for nome in num_favoritos.keys():                     # ATRIBUI À VARIÁL nome OS VALORES de NOME
+    print('O nome é ' + str(nome).title())            # MOSTRA O VALOR key e values
+print('\n')
 
