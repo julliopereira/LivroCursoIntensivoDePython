b531fef0ca93e391ee8c7f7cd5214da297039001
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-24 16:40:01 -0300

    Auto Atualização para GitHub

---
+++ 0042_aninhadas_Lista_com_dicionarios.py
@@ -0,0 +1,18 @@
+### EXPLICAÇÃO.
+
+'''
+Muitas vezes você quer adicionar um dicionarío como valor de uma lista, 
+ou seja, em uma posição de uma lista você pode ter várias informações 
+contidas em um dicionário.
+
+#DICIONARIOS:
+
+pessoa_1 = {'nome': 'rodrigo',}
+pessoa_2 = {}
+pessoa_3 = {}
+
+#ADICIONANDO Ã LISTA:
+
+lista = (pessoa_1, pessoa_2, pessoa_3)
+
+'''
