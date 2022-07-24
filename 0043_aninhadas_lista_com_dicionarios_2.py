238f6dc1d66886b39bdb9ba9a026d481925cc0fc
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-24 17:15:01 -0300

    Auto Atualização para GitHub

--- 0043_aninhadas_lista_com_dicionarios_2.py
+++ 0043_aninhadas_lista_com_dicionarios_2.py
@@ -1,26 +1,3 @@
-### EXPLICAÇÃO.
-
-'''
-Muitas vezes você quer adicionar um dicionarío como valor de uma lista, 
-ou seja, em uma posição de uma lista você pode ter várias informações 
-contidas em um dicionário.
-
-'''
-
-# DICIONARIOS:
-
-pessoa_1 = {'nome': 'rodrigo', 'idade': '20', 'altura': '1.70', 'peso': '70'}
-pessoa_2 = {'nome': 'maria', 'idade': '24', 'altura': '1.66', 'peso': '65'}
-pessoa_3 = {'nome': 'sara', 'idade': '23', 'altura': '1.68', 'peso': '71'}
-
-# ADICIONANDO Ã LISTA:
-
-lista = (pessoa_1, pessoa_2, pessoa_3)
-for pessoa in lista:
-    print(pessoa)
-
-print('-'*50) #---------------------------------------------------------------------------------------
-
 # AQUI VAMOS AGORA MONTAR ALGO PARECIDO PORÉM COM 30 PESSOAS
 
 pessoas = []                                         # LISTA VAZIA
@@ -40,3 +17,5 @@ for pessoa in pessoas[:5]:                           # Vai mostrar as 5 primeira
 print('-'*50) #---------------------------------------------------------------------------------------
 
 print('Todal de pessoas: ' + str(len(pessoas)))      # Mostra a quantidade de pessoas
+
+print(pessoas)
