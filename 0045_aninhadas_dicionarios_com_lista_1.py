d31893fbb982ca556f194c62495371f5af0aefe1
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-25 13:45:01 -0300

    Auto Atualização para GitHub

--- 0045_aninhadas_dicionarios_com_lista_1.py
+++ 0045_aninhadas_dicionarios_com_lista_1.py
@@ -9,12 +9,12 @@ linguagens_favoritas = {                            # CRIAR DISSIONARIO ONDE OS 
 
 for nome, linguagem in linguagens_favoritas.items():                        # UM FOR PARA BUSCAR keys E values
     if len(linguagem) > 1:                                                  # SE O TAMANHO DA LISTA FOR MAIOR QUE 1 ITEM 
-        print(nome.title() + ', as linguagens que você escolheu são: ')
-        for ling in linguagem: 
+        print(nome.title() + ', as linguagens que você escolheu são: ')         # NO PLURAL
+        for ling in linguagem:                                  
             print('\t',ling)
             
-    elif len(linguagem) == 1:
-        print(nome.title() + ', a linguaguem que você escolheu é: ')
+    elif len(linguagem) == 1:                                               # SE O TAMANHO DA LISTA FOR IGUAL A 1 ITEM 
+        print(nome.title() + ', a linguaguem que você escolheu é: ')            # NO SINGULAR
         for ling in linguagem: 
             print('\t',ling)
     
