81f8c710495905a097da76f11314e1690718901f
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-25 12:55:01 -0300

    Auto Atualização para GitHub

---
+++ 0045_aninhadas_dicionarios_com_lista_1.py
@@ -0,0 +1,14 @@
+### USANDO DICIONÁRIOS DENTRO DE LISTAS
+
+linguagens_favoritas = {
+    'joao': ['python', 'c'] 
+    'sara': ['c', 'js']
+    'eduardo': ['ruby', '']
+    'jorge': 'python',
+    'roberto': 'python',
+    '': 'python',
+    'phil': 'python',
+    'phil': 'python',
+    'phil': 'python',
+    'phil': 'python',
+    }
