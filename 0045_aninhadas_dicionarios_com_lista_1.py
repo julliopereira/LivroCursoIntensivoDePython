1946a2e449eabc7b6b7ed9ebb986e3a85db15f1e
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-25 13:40:01 -0300

    Auto Atualização para GitHub

--- 0045_aninhadas_dicionarios_com_lista_1.py
+++ 0045_aninhadas_dicionarios_com_lista_1.py
@@ -1,18 +1,20 @@
-### USANDO DICIONÁRIOS DENTRO DE LISTAS
+### USANDO LISTAS DENTRO DE DICIONARIOS
 
-linguagens_favoritas = {
+linguagens_favoritas = {                            # CRIAR DISSIONARIO ONDE OS values SÃO LISTAS
     'joao': ['python', 'c'],
     'sara': ['c'],
     'eduardo': ['ruby', 'java'],
     'jorge': ['python'],
     }
 
-for nome, linguagem in linguagens_favoritas.items():
-    if len(linguagem) > 1:
-        print(nome,'As linguagens que você escolheu são: ')
+for nome, linguagem in linguagens_favoritas.items():                        # UM FOR PARA BUSCAR keys E values
+    if len(linguagem) > 1:                                                  # SE O TAMANHO DA LISTA FOR MAIOR QUE 1 ITEM 
+        print(nome.title() + ', as linguagens que você escolheu são: ')
         for ling in linguagem: 
             print('\t',ling)
             
     elif len(linguagem) == 1:
-        print(nome,'a linguaguem que você escolheu é: ',linguagem)
+        print(nome.title() + ', a linguaguem que você escolheu é: ')
+        for ling in linguagem: 
+            print('\t',ling)
     
