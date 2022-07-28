ef070fb414f1bef0f172ec30d6933d9dab481b0f
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-28 20:30:01 -0300

    Auto Atualização para GitHub

--- 0046_aninhados_exercicio_livro1_.py
+++ 0046_aninhados_exercicio_livro1_.py
@@ -4,3 +4,34 @@
 # ARMAZENE OS TRES DICIONARIOS EM UMA LISTA CHAMADA people. PERCORRA SUA LISTA DE PESSOAS COM UM LAÇO.
 # A MEDIDA QUE PERCORRER A LISTA APRESENTE TUDO QUE VOCÊ SABE SOBRE CADA PESSOA
 
+pessoas = { 
+    'mario' = { 
+        'nome': 'mario',
+        'sobrenome': 'ayala',
+        'altura': '1.80',
+        'peso': '80',
+        'civil': 'solteiro',
+        'esporte': 'futebol',
+        'trabalho': 'engenheiro',
+        }
+
+    'maria' = { 
+        'nome': 'maria',
+        'sobrenome': 'mendes',
+        'altura': '1.65',
+        'peso': '60',
+        'civil': 'solteira',
+        'esporte': 'voley',
+        'trabalho': 'tecnica',
+        }
+
+    'julio' = { 
+        'nome': 'julio',
+        'sobrenome': 'pereira',
+        'altura': '1.84',
+        'peso': '90',
+        'civil': 'solteiro',
+        'esporte': 'futebol',
+        'trabalho': 'Adm de rede',
+        }
+    }
