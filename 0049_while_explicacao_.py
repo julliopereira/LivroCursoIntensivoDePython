aec8e513043e72c4986f581407af1af580de3d41
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-30 17:00:01 -0300

    Auto Atualização para GitHub

---
+++ 0049_while_explicacao_.py
@@ -0,0 +1,25 @@
+### DIFERENTE DO LAÇO for O LAÇO while EXECUTA ENQUANTO O USUARIO QUISER PERMANECER NELE
+
+contador = 1
+
+while contador <= 5:                                        # SE O CANTADOR MENOR OU IGUAL A CINCO FAÇA O QUE ESTÁ DENTRO DE while
+    print('contador: ' + str(contador) + ' menor que 6')
+    contador += 1
+
+
+print("-"*50) #---------------------------------------------------------------------
+#### USURIO VAI DIGITAR VÁRIAS PALAVRAS A SEREM MOSTRADAS NA TELA, MAS SÓ VAI SAIR DO while QUANDO DIGITAR quit
+
+dig = input('Digite qualquer coisa para eu mostrar')
+
+print("-"*50) #---------------------------------------------------------------------
+#### DEIXANDO O USUARIO SAIR QUANDO QUISER
+
+print("-"*50) #---------------------------------------------------------------------
+#### DEIXANDO O USUARIO SAIR QUANDO QUISER
+
+print("-"*50) #---------------------------------------------------------------------
+#### DEIXANDO O USUARIO SAIR QUANDO QUISER
+
+print("-"*50) #---------------------------------------------------------------------
+#### DEIXANDO O USUARIO SAIR QUANDO QUISER
--- 0049_while_explicacao_basica_.py
+++
@@ -1,7 +0,0 @@
-### DIFERENTE DO LAÇO for O LAÇO while EXECUTA ENQUANTO O USUARIO QUISER PERMANECER NELE
-
-contador = 1
-
-while contador <= 5:                                        # SE O CANTADOR MENOR OU IGUAL A CINCO FAÇA O QUE ESTÁ DENTRO DE while
-    print('contador: ' + str(contador) + ' menor que 6')
-    contador += 1
