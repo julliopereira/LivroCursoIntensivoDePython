b9987d884cd000f323fb7c254f0211051d0422fe
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-30 15:35:01 -0300

    Auto Atualização para GitHub

---
+++ 0048_input_explicacao_.py
@@ -0,0 +1,8 @@
+### A FUNÇÃO input() É UTILIZADA PARA SOLICATAR UMA ENTRADA DO USUÁRIO
+### UTILIZAMOS UMA VARIÁVEL PARA GUARDAR O VALOR DIGITADO PELO USUÁRIO
+
+variavel = input('Digite qualquer coisa: ')             # SOLICITA AO USUARIO UMA ENTRADA E GUARDA NA VARIAVEL variavel
+print(variavel)
+
+print('-'*50)
+
