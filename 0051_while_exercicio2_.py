0bc8ab7fd0432beedf84a8fe9fd0690189fca108
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-30 21:25:01 -0300

    Auto Atualização para GitHub

--- 0051_while_exercicio2_.py
+++ 0051_while_exercicio2_.py
@@ -1,1 +1,5 @@
-### DIFERENTES VALORES DE 
+### DIFERENTES VALORES DE PREÇO DE INGRESSO DE ACORDO COM IDADE
+### 3 anos = gratuito | 3-12 = 10 dolares | 13 - = 15 dolares
+
+while True:
+    idade = 
