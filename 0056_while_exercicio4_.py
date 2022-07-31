eaf5469bd8300a3a3bf7e61fc6f9b3b6cdf1b5ac
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-31 18:15:01 -0300

    Auto Atualização para GitHub

--- 0056_while_exercicio4_.py
+++ 0056_while_exercicio4_.py
@@ -16,8 +16,22 @@ for sandwich in finished_sandwiches:
     print('\tSandwitch pronto: ' + sandwich.title())
 
 
+print('-'*50) #-----------------------------------------------------------------------------------
 ### INFORMAR AOS CLIENTES QUE A LANCHONETE ESTA SEM pastrami E REMOVER DA LISTA
 
+sandwich_orders = ['tomate', 'pastrami', 'pichles', 'pastrami', 'queijo prato', 'alface', 'picanha', 'ovo', 'pastrami']
+finished_sandwiches = []
+
 while 'pastrami' in sandwich_orders:
-    sand
+    sandwich_orders.remove('pastrami')
+
+print('\nPreparando os lanches: ')
+while sandwich_orders:
+    sandwich = sandwich_orders.pop()
+    print('\tO sandwich de ' + sandwich.title() + ' foi preparado.')
+    finished_sandwiches.append(sandwich)
+
+print('\nSandwitchs finalizados: ')
+for sandwich in finished_sandwiches:
+    print('\tSandwitch pronto: ' + sandwich.title())
 
