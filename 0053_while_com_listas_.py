adcca822c20be9f24e9e68bbfba4386e51539d12
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-31 16:35:01 -0300

    Auto Atualização para GitHub

--- 0053_while_com_listas_.py
+++ 0053_while_com_listas_.py
@@ -1,3 +1,10 @@
 ### ALTERAR USUARIOS NÃO CONFIRMADOS PARA CONFIRMADOS 
 
-unconfirmed_users = {'alice', 'brian'}
+unconfirmed_users = {'alice', 'brian', 'candace'}           # CRIADO DICIONARIO COM OS NOMES
+confirmed_users = []                                        # CRIADO LISTA
+
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print('Verificando usuario: ' + current_user + '!.')
+    confirmed_users.append() = current_user
+    
