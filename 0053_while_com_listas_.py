1a9387e9f4d6d2a4a551c9f59beab854314b5e70
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-31 16:40:01 -0300

    Auto Atualização para GitHub

--- 0053_while_com_listas_.py
+++ 0053_while_com_listas_.py
@@ -3,8 +3,10 @@
 unconfirmed_users = {'alice', 'brian', 'candace'}           # CRIADO DICIONARIO COM OS NOMES
 confirmed_users = []                                        # CRIADO LISTA
 
-while unconfirmed_users:
-    current_user = unconfirmed_users.pop()
+while unconfirmed_users:                                    # ENQUANTO EXISTIR UM USUARIO NO DICIONARIO
+    current_user = unconfirmed_users.pop()                  # RETIRA O ULTIMO USUARIO DO DICIONARIO E GRAVA O NOME NA VARIAVEL current_user
     print('Verificando usuario: ' + current_user + '!.')
-    confirmed_users.append() = current_user
-    
+    confirmed_users.append(current_user)                    # ADICIONA O USUARIO NA LISTA confirmed_users
+
+for confirmed_user in confirmed_users:
+    print('O usuário ' + confirmed_user + )
