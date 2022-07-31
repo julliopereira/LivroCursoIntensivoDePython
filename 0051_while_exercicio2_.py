a40e3410d1cdcaf9a0a9034baa00e715d74c8113
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-30 21:30:01 -0300

    Auto Atualização para GitHub

--- 0051_while_exercicio2_.py
+++ 0051_while_exercicio2_.py
@@ -1,5 +1,16 @@
 ### DIFERENTES VALORES DE PREÇO DE INGRESSO DE ACORDO COM IDADE
 ### 3 anos = gratuito | 3-12 = 10 dolares | 13 - = 15 dolares
 
+print('Digite sua idade para obter seu ingresso: \n')
+
 while True:
-    idade = 
+    idade = int(input('\t Digite a idade: '))
+    if idade <= 3:
+        print('O ingresso é gratuito')
+        break
+    elif idade <= 12:
+        print('O valor do ingresso é 10 dolares')
+        break
+    else:
+        print('O valor do ingresso é 15 dolares')
+        break
