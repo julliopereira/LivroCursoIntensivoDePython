1f670c87790201ac12f5ed23fffdae831ecfbddf
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-31 17:55:01 -0300

    Auto Atualização para GitHub

--- 0056_while_exercicio4_.py
+++ 0056_while_exercicio4_.py
@@ -2,5 +2,14 @@
 ### MOSTRAR O NOME A MEDIDA QUE FOREM PREPARADOS
 
 
-sandwich_orders = ['tomate', 'pichles', 'queijo prato', ]
+sandwich_orders = ['tomate', 'pichles', 'queijo prato', 'alface', 'picanha', 'ovo']
+finished_sandwiches = []
+
+while sandwich_orders:
+    sandwich = sandwich_orders.pop()
+    print('O sandwich ' + sandwich + 'foi preparado.')
+    finished_sandwiches.append(sandwich)
+
+for sandwich 
+
 
