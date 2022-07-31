792265e0bc02cac197e951f2a88e4883bcbaf4b6
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-31 17:10:02 -0300

    Auto Atualização para GitHub

--- 0055_while_dicionario_preencher_.py
+++ 0055_while_dicionario_preencher_.py
@@ -1,9 +1,15 @@
 ### SOLICITA NOME DO PARTICIPANTE E UMA RESPOSTA ; USAR FLAG 
 
+from timeit import repeat
+
+
 responses = {}
 
 polling_active = True
 
-while polling_active:
-    nome = input(\nQual é o seu nome? :)
-    
+while polling_active:                                                   # ENQUANTO VERDADEIRO
+    nome = input('\nQual é o seu nome? : ')
+    response = input('você gostaria de escalar qual montanha? : ')
+    responses[nome] = response                                          # ADICIONA A RESPOSTA (value) NA CHAVE (key) NOME
+    repeat = input('\nVocê quer que outra pessoa responda? [si/no]')
+
