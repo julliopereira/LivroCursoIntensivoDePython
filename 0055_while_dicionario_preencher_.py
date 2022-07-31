bb4887cf0c69c08cd9e72da54d2584106fc3c8cb
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-31 17:20:01 -0300

    Auto Atualização para GitHub

--- 0055_while_dicionario_preencher_.py
+++ 0055_while_dicionario_preencher_.py
@@ -12,5 +12,9 @@ while polling_active:                                                   # ENQUAN
     response = input('você gostaria de escalar qual montanha? : ')
     responses[nome] = response                                          # ADICIONA A RESPOSTA (value) NA CHAVE (key) NOME
     repeat = input('\nVocê quer que outra pessoa responda? [si/no]')
-    if repeat == no:
+    if repeat == 'no':
         polling_active = False
+
+print('\n--- POOL RESULTS ---')
+for k, v in responses.items():
+    print(k + 'gostaria de escalar ' + v)
