c68a4952dbcaf6215b5ed18f01510eec2e50e433
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-30 22:00:01 -0300

    Auto Atualização para GitHub

--- 0052_while_exercicio3_.py
+++ 0052_while_exercicio3_.py
@@ -1,1 +1,20 @@
-### 
+### USAR TODOS OS TERMOS APRENDIDOS
+
+ingr = ''
+
+print('Digite os ingredientes para a pizza: \n')
+
+
+while ingr != 'quit':
+    ingr = input('\tIngrediente: ')
+    if ingr == 'quit':
+        break
+
+while True:
+    pagar = input('Como deseja pagar ?')
+    if pagar != '':
+        continue
+    else:
+        break
+
+print('Ótimo pedido, estamos preparando sua pizza...')
