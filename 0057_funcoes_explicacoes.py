7e56cc1f4283c2bf70bc374d5ba9407ac6251651
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-31 22:00:01 -0300

    Auto Atualização para GitHub

--- 0057_funcoes_explicacoes.py
+++ 0057_funcoes_explicacoes.py
@@ -1,5 +1,6 @@
 ### FUNÇÃO É UM CONJUNTO DE INSTRUÇÕES QUE PODE SER CHAMADO A QUALQUER MOMENTO 
 ### [01] SIMPLES:
+print('EX: 01 \n')
 
 def func_saudacao():
     """Exibe saudação simples."""                           # DESCRIÇÃO DA FUNÇÃO UTILIZADA EM PYTHON
@@ -9,6 +10,7 @@ func_saudacao()                                             # CHAMANDO A FUNÇÃ
 
 print('-'*50) #-----------------------------------------------------------------------------
 ### [02] PASSANDO ARGUMENTO PARA FUNÇÃO:
+print('EX: 02 \n')
 
 def func_saudacao2(username):                               # NECESSÁRIO ENCAMINHAR UM ARGUMENTO PARA FUNÇÃO
     """Exibe saudação com passagem de informações"""
@@ -18,6 +20,7 @@ func_saudacao2('julio')                                     # PASSANDO O ARGUMEN
 
 print('-'*50) #-----------------------------------------------------------------------------
 ### [03] ARGUEMENTOS POSICIONAIS:
+print('EX: 03 \n')
 
 def func_animal_tipo(tipo, nome):
     """Exibe informações sobre animal """
@@ -28,6 +31,7 @@ func_animal_tipo('hamster', 'bidú')
 
 print('-'*50) #-----------------------------------------------------------------------------
 ### [04] VÁRIAS CHAMADAS DA MESMA FUNÇÃO:
+print('EX: 04 \n')
 
 func_animal_tipo('dog', 'lessi')                    # NESTE EXEMPLO A ORDEM IMPORTA
 func_animal_tipo('cat', 'white')
@@ -36,21 +40,27 @@ func_animal_tipo('cavalo', 'fogo')
 
 print('-'*50) #-----------------------------------------------------------------------------
 ### [05] ALTERANDO A ORDEM  :
+print('EX: 05 \n')
 
 func_animal_tipo('boi', 'malhado')
 func_animal_tipo(nome='fogo', tipo='cavalo')                # AQUI ALTERADA ORDEM NO ENTANDO FOI ESPECIFICADO O ARGUMENTO  
 
 print('-'*50) #-----------------------------------------------------------------------------
-### [05] VALORES DEFAULT  :
+### [06] VALORES DEFAULT  :
+print('EX: 06 \n')
 
-def func_animal_tipo2(tipo='dog', nome):                    # SE NENHUM ARGUMENTO PARA tipo FOR ENTÃO É MOSTRADA OP
-    """Exibe informações sobre animal """
-    print('\n\tEu tenho um ' + tipo.title())
-    print('\tE o nome dele é ' + nome.title() + '\n')
+def func_animal_tipo2(nome_pet, tip='dog'):                 # SE NENHUM ARGUMENTO PARA tipo FOR ENTÃO É MOSTRADA OPÇÃO DEFAULT = dog               
+    """Exibe informações sobre animal """                   # NECESSÁRIO QUE O DEFAULT SEJA O ÚLTIMO VALOR DA FUNÇÃO.
+    print('\n\tEu tenho um ' + tip.title())
+    print('\tE o nome dele é ' + nome_pet.title() + '\n')
 
-func_animal_tipo2('hamster', 'bidú')
+func_animal_tipo2('white')
+func_animal_tipo2('malhado')
+func_animal_tipo2(tip='cavalo', nome_pet='fogo')
 
 print('-'*50) #-----------------------------------------------------------------------------
 ### [07] MAIS ARGUMENTOS QUE O ESPERADO PELA FUNÇÃO:
+print('EX: 07')
+
 
 func_animal_tipo('dog', 'lessi', '10kG')                    # VAI DAR ERRO, A FUNÇÃO ESPERA APENAS 2
