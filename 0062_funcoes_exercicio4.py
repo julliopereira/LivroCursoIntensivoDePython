c06e6aa57d0ca1dd29999d7f5299a9ab968f1ed3
Julio Cesar Pereira <julliopereira@gmail.com>
2022-08-01 20:30:01 -0300

    Auto Atualização para GitHub

--- 0062_funcoes_exercicio4.py
+++ 0062_funcoes_exercicio4.py
@@ -1,8 +1,16 @@
 ### ALBUM
 
-def func_make_album(n_artista, n_album):
-    n_artista['artista'] = n_artista
-    n_artista['album'] = n_album
+def func_make_album(n_artista, n_album, n_faixas):
+    n_artista = {'artista':n_artista, 'album':n_album, 'faixas': n_faixas }
     return n_artista
 
-print(func_make_album('maria', 'florentina'))
+artista = input('Digite artista: ')
+album = input('Digite Album: ')
+faixas = input('Numero de faixas: ')
+
+albuns = func_make_album(artista, album, faixas)
+
+
+for k, v in albuns.items():
+    print('\n' + k + ':' + v)
+
