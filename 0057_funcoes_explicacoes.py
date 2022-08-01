1b118131108443fb7f5f3dc6a8ff4a6a143fcbda
Julio Cesar Pereira <julliopereira@gmail.com>
2022-07-31 21:30:02 -0300

    Auto Atualização para GitHub

--- 0057_funcoes_explicacoes.py
+++ 0057_funcoes_explicacoes.py
@@ -21,7 +21,36 @@ print('-'*50) #-----------------------------------------------------------------
 
 def func_animal_tipo(tipo, nome):
     """Exibe informações sobre animal """
-    print('\n\tEu tenho um pet ' + tipo.title())
+    print('\n\tEu tenho um ' + tipo.title())
     print('\tE o nome dele é ' + nome.title() + '\n')
 
 func_animal_tipo('hamster', 'bidú')
+
+print('-'*50) #-----------------------------------------------------------------------------
+### [04] VÁRIAS CHAMADAS DA MESMA FUNÇÃO:
+
+func_animal_tipo('dog', 'lessi')                    # NESTE EXEMPLO A ORDEM IMPORTA
+func_animal_tipo('cat', 'white')
+func_animal_tipo('boi', 'malhado')
+func_animal_tipo('cavalo', 'fogo')
+
+print('-'*50) #-----------------------------------------------------------------------------
+### [05] ALTERANDO A ORDEM  :
+
+func_animal_tipo('boi', 'malhado')
+func_animal_tipo(nome='fogo', tipo='cavalo')                # AQUI ALTERADA ORDEM NO ENTANDO FOI ESPECIFICADO O ARGUMENTO  
+
+print('-'*50) #-----------------------------------------------------------------------------
+### [05] VALORES DEFAULT  :
+
+def func_animal_tipo2(tipo='dog', nome):                    # SE NENHUM ARGUMENTO PARA tipo FOR ENTÃO É MOSTRADA OP
+    """Exibe informações sobre animal """
+    print('\n\tEu tenho um ' + tipo.title())
+    print('\tE o nome dele é ' + nome.title() + '\n')
+
+func_animal_tipo2('hamster', 'bidú')
+
+print('-'*50) #-----------------------------------------------------------------------------
+### [07] MAIS ARGUMENTOS QUE O ESPERADO PELA FUNÇÃO:
+
+func_animal_tipo('dog', 'lessi', '10kG')                    # VAI DAR ERRO, A FUNÇÃO ESPERA APENAS 2
