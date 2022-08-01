860f94f4c822b6a738449b3974120281a8e97cbf
Julio Cesar Pereira <julliopereira@gmail.com>
2022-08-01 20:00:01 -0300

    Auto Atualização para GitHub

---
+++ 0062_funcoes_exercicio4.py
@@ -0,0 +1,8 @@
+### ALBUM
+
+def func_make_album(n_artista, n_album):
+    n_artista['artista'] = n_artista
+    n_artista['album'] = n_album
+    return n_artista
+
+print(func_make_album('maria', 'florentina'))
