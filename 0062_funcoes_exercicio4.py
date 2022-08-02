be35a2a7e5fc854cdf21c86af9f79f045019b482
Julio Cesar Pereira <julliopereira@gmail.com>
2022-08-01 21:00:01 -0300

    Auto Atualização para GitHub

--- 0062_funcoes_exercicio4.py
+++ 0062_funcoes_exercicio4.py
@@ -1,8 +1,13 @@
 ### ALBUM
 
+
+
+
 def func_make_album(n_artista, n_album, n_faixas):
     n_artista = {'artista':n_artista, 'album':n_album, 'faixas': n_faixas }
     return n_artista
+
+def func_add_dados(n_artista, n_album, n_faixas)
 
 artista = input('Digite artista: ')
 album = input('Digite Album: ')
