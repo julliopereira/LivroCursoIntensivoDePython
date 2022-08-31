eb3d53b114d2087b9b753c566c8cb1b09c4ad628
Julio Cesar Pereira <julliopereira@gmail.com>
2022-08-31 00:00:02 -0300

    Auto Atualização para GitHub

--- 0068_funcoes_qde_argumentos_nomeados.py
+++ 0068_funcoes_qde_argumentos_nomeados.py
@@ -1,10 +1,13 @@
-### 
+### ADICIONA ATRIBUTOS OU INFORMACOES DIVERSAS DE TAMANHO INDEFINIDO
 
-def build_profile(first, last, **user_info):
+def build_profile(first, last, **user_info):                    # **user_info - CRIA UM DICIONARIO VAZIO 
     """Controi um dicionario com tudo o que sabemos do usuario"""
     profile = {}
-    profile['first_name'] = first
-    profile['last_name'] = last
-    for key, value in profile.items():
+    profile['first_name'] = first.title()
+    profile['last_name'] = last.title()
+    for key, value in user_info.items():
         profile[key] = value
-        
+    return profile
+
+user_profile = build_profile('albert', 'eistein', local='alemanha', atuacao='fisico', descoberta='relatividade')
+print(user_profile)
---
+++ 0069_funcoes_qde_arg_exercicio_8.py
@@ -0,0 +1,9 @@
+### LISTA DE INTENS EM SANDWICH 
+
+def sandwichs(*items):
+    sandwich = []
+    for item in items:
+        sandwich.append(item) 
+    print(sandwich)
+
+sandwichs('tomate', 'queijo', 'alface', 'hamburguer', 'maionase', 'catchup', 'mostarda', 'picles')
