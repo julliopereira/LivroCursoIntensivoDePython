b698b7b42ea12d4a17191777ecaa3be79e9fd008
Julio Cesar Pereira <julliopereira@gmail.com>
2022-08-31 20:00:01 -0300

    Auto Atualização para GitHub

--- 0070_funcoes_modulos_importando.py
+++ 0070_funcoes_modulos_importando.py
@@ -1,3 +1,13 @@
 ### VAMOS CRIAR O ARQUIVO pizza.py QUE É NOSSO MODULO
 ### DEPOIS VAMOS CRIAR OUTRO making_pizzas.py QUE VAI CHAMAR O MODULO make_pizza.py
 ### ISSO TUDO NO DIRETORIO MODULOS
+
+"""
+PARA IMPORTAR:
+
+import nome_do_modulo
+
+ou 
+
+from nome_do_modulo import nome_da_funcao
+"""
--- MODULOS/making_pizzas.py
+++ MODULOS/making_pizzas.py
@@ -1,3 +1,5 @@
+### IMPORTAR MODULO E USAR nome_do_modulo.nome_da_funcao()
+
 import pizza  #importa as funcoes que estao no arquivo pizza.py
 
 pizza.make_pizza(16, 'peperoni')
