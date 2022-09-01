26ede0dcc82efe36fcdbf3918c1725a60658fe49
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-01 16:30:01 -0300

    Auto Atualização para GitHub

---
+++ CLASSES/dog.py
@@ -0,0 +1,17 @@
+class Dog():
+    """Uma tentativa de modelar um cachorro"""
+    def __init__(self,name, age):
+        """Inicializa os atributos name e age."""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """Simula um cachorro sentando"""
+        print(f'{self.name.title()} sente!')
+
+    def roll_over(self):
+        """Simula um cachorro rolando"""
+        print(f'{self.name.title()} rola!')
+
+cachorro = Dog('wille', 14)
+print(f'{cachorro.name.title()}')
