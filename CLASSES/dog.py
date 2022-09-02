11e031e4f00b83e1ccfa5297cb6e3f5b12ef9830
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-02 00:00:02 -0300

    Auto Atualização para GitHub

--- CLASSES/dog.py
+++ CLASSES/dog.py
@@ -1,17 +1,25 @@
 class Dog():
     """Uma tentativa de modelar um cachorro"""
-    def __init__(self,name, age):
+    def __init__(self,name, age):                   # ATRIBUTOS
         """Inicializa os atributos name e age."""
         self.name = name
         self.age = age
 
-    def sit(self):
-        """Simula um cachorro sentando"""
+    def sit(self):                                  # METODO
+        """Simula cachorro sentando"""
         print(f'{self.name.title()} sentar!')
 
-    def roll_over(self):
-        """Simula um cachorro rolando"""
+    def roll_over(self):                            # METODO
+        """Simula cachorro rolando"""
         print(f'{self.name.title()} rolar!')
 
-cachorro = Dog('wille', 14)
-print(f'Meu cachorro é o {cachorro.name.title()} e tem a idade de {cachorro.age} anos.')
+my_dog = Dog('wille', 14)                           # VARIAVEL NESTE CASO É UMA > INSTANCIA
+print(f'Meu cachorro é o {my_dog.name.title()} e tem a idade de {my_dog.age} anos.')
+my_dog.sit()
+my_dog.roll_over()
+
+your_dog = Dog('Lucy', 3)                           # VARIAVEL NESTE CASO É UMA > INSTANCIA
+print(f'\nSua calinha {your_dog.name.title()} tem {your_dog.age} anos. ')
+your_dog.sit()
+your_dog.roll_over()
+
---
+++ CLASSES/restaurant.py
@@ -0,0 +1,10 @@
+class Restaurant():
+    def __init__(self,restaurant_name,cousine_type):
+        self.restaurant_name = restaurant_name
+        self.cousine_type = cousine_type
+
+    def describe_restautant(self):
+        print(f'Voc{}')
+
+    def open_restaurant(self):
+        
