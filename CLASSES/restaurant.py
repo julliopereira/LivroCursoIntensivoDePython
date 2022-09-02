ded20d35ab727c72c3e07cdf3320116adac3defc
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-02 00:30:01 -0300

    Auto Atualização para GitHub

--- CLASSES/restaurant.py
+++ CLASSES/restaurant.py
@@ -4,7 +4,13 @@ class Restaurant():
         self.cousine_type = cousine_type
 
     def describe_restautant(self):
-        print(f'Voc{}')
+        print(f'Você está no restaurante {self.restaurant_name.title()}')
+        print(f'Servimos comida {self.cousine_type.title()}')
 
     def open_restaurant(self):
-        
+        print(f'o restaurante {self.restaurant_name.title()} está aberto!!!')
+
+restaurant = Restaurant('bodão', 'fast food')
+print(f'{restaurant.restaurant_name.title()} {restaurant.cousine_type.title()}')
+restaurant.describe_restautant()
+restaurant.open_restaurant()
