cbc0e3266ce57c07dd51ef128880c8fa1aa21986
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-15 18:30:01 -0300

    Auto Atualização para GitHub

--- ARQUIVOS_E_EXCECOES/file_reader_lista_de_linhas.py
+++ ARQUIVOS_E_EXCECOES/file_reader_lista_de_linhas.py
@@ -1,7 +1,11 @@
- LER O ARQUIVO pi_digits.txt
+# LER O ARQUIVO pi_digits.txt
 
-print(f'MOSTRA CONTEUDO DO ARQUIVO MAS COM ESPACOS:')
+print(f'MOSTRA CONTEUDO DO ARQUIVO SEM ESPACOS:')
 file_path = '/home/julio/Documentos/git/LivroCursoIntensivoDePython/ARQUIVOS_E_EXCECOES'
-with open(file_path + '/pi_digits.txt') as file_object:      # ABRIR AQUIVO pi_digits.txt 
-    for line in file_object:
-        print(line)                                          # MOSTRAR CONTEÚDO DE line
+with open(file_path + '/pi_digits.txt') as file_object:           # ABRIR AQUIVO pi_digits.txt 
+    lines = file_object.readlines()                               # ARMAZENA CADA LINHA DE ARQUIVO EM UMA LISTA
+
+print(lines)                                                      # MOSTRA A LISTA COMPLETA
+
+for line in lines:
+    print(line.rstrip())                                          # MOSTRAR CONTEÚDO DE line SEM ESPACOS
