6f1c58ced6b6cbbb58c8040e407f9aecffa4cd9a
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-15 20:30:01 -0300

    Auto Atualização para GitHub

--- ARQUIVOS_E_EXCECOES/lendo_learning_python.py
+++ ARQUIVOS_E_EXCECOES/lendo_learning_python.py
@@ -1,4 +1,20 @@
 # EXERCICIO PARA LER O ARQUIVO learning_python.txt
 
 arq = '/home/julio/Documentos/git/LivroCursoIntensivoDePython/ARQUIVOS_E_EXCECOES/learning_python.txt'
-with open('arq') as arquivo
+with open(arq) as arquiv:
+    arquivo = arquiv.read()
+    print(arquivo)
+
+    for line in arquiv:
+        print(line.rstrip())
+
+"""
+with open(arq) as arqu:
+    arquivo = arqu.readlines()
+    for line in arquivo:
+        print(line)
+"""
+
+
+
+
