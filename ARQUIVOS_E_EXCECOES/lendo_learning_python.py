7797a1c3f9e8d010521ff68b86211798e68990c1
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-15 20:00:01 -0300

    Auto Atualização para GitHub

---
+++ ARQUIVOS_E_EXCECOES/lendo_learning_python.py
@@ -0,0 +1,4 @@
+# EXERCICIO PARA LER O ARQUIVO learning_python.txt
+
+arq = '/home/julio/Documentos/git/LivroCursoIntensivoDePython/ARQUIVOS_E_EXCECOES/learning_python.txt'
+with open('arq') as arquivo
