d9dbe11ebe34fa11dedbb0257802972db6b0f033
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-16 19:30:01 -0300

    Auto Atualização para GitHub

--- ARQUIVOS_E_EXCECOES/exercicio_10.3_guest.py
+++ ARQUIVOS_E_EXCECOES/exercicio_10.3_guest.py
@@ -4,11 +4,13 @@ nome = input('Digite seu nome: ')
 arq = input('Digite o nome do arquivo: ')
 path = ('/home/julio/Documentos/git/LivroCursoIntensivoDePython/ARQUIVOS_E_EXCECOES/' + arq)
 
-with open(path, 'a') as f_obj:
-    f_obj.write(nome)
+with open(path, 'a') as f_obj:                      # OCAO a NAO APAGA O CONTEUDO EXISTENTE ADICIONANDO NOVOS VALORES
+    f_obj.write('\t\t')
+    f_obj.write(nome)                               # CONTEÚDO EM name 
+    f_obj.write('\n')                               # PROXIMA LINHA
 
 with open(path) as file_obj:
     arquivo = file_obj.read()
-    print('\n\nSeu nome é: \n' + arquivo.)
+    print('\n\n\tSeu nome é: \n' + arquivo.rstrip())
 
 
