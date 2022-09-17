813cca9db920965b0298e65f2b1d89ad6f950d6d
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-17 00:00:02 -0300

    Auto Atualização para GitHub

--- ARQUIVOS_E_EXCECOES/alice_excecao_filenotfounderror.py
+++ ARQUIVOS_E_EXCECOES/alice_excecao_filenotfounderror.py
@@ -6,4 +6,10 @@ try:
         contents = f_obj.read()
 except FileNotFoundError:
     print(f'\n\t- Desculpe, Arquivo "{filename}" inexistente!')
+else:
+    # Conta o numero aproximado de palavras no arquivo
+    palavras = contents.split()
+    num_palavras = len(palavras)
+    print(f'\n\t- A quantidade de palavras no texto é: {str(num_palavras)} \n')
+
 
