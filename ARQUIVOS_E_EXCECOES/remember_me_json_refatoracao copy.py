addd84a05c914a412af4f7de7bf1d50fa35c4e78
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-19 00:30:01 -0300

    Atualização para GitHub

--- ARQUIVOS_E_EXCECOES/remember_me_json_refatoracao copy.py
+++ ARQUIVOS_E_EXCECOES/remember_me_json_refatoracao copy.py
@@ -2,18 +2,27 @@
 
 import json
 
+def get_stored_username():
+    """Obtem o nome ja armazenado se estiver disponviel"""
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:                          
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username 
+
 def greet_user():
     """Saúda o usuário pelo nome"""
-    filename = input('Digite seu username: ')
-    try:
-        with open(filename) as f_obj:                          # GRAVA NOME DO ARQUIVO EM f_obj
-            username = json.load(f_obj)                        # CARREGA O CONTEUDO DE f_obj NA VARIAVEL USERNAME 
-    except FileNotFoundError:                                  # EXECUTA O BLOCO ABAIXO SE O ARQUIVO NAO EXISTIR
+    username = get_stored_username()
+    if username:
+        print(f'Bem vindo de volta "{username}"')
+    else:
         username = input('Qual o seu username: ')              # PEDE AO USUARIO UM USERNAME
+        filename = 'username.json'
         with open(filename, 'w') as f_obj:                     # ABRE O ARQUIVO EM MODO DE GRAVAÇÃO
             json.dump(username, f_obj)                         # GRAVA O CONTEUDO DA VARIAVEL username EM f_obj
             print(f'\n\tNós relembraremos você "{username}".') 
-    else:
-        print(f'\n\tBem vindo de volta "{username}"!')         # SE O ARQUIVO EXISTIR EM try ENTAO RELEMBRA O username
 
 greet_user()
