151fab718469aeb42edf35bdec4a5cbcfda444e3
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-25 21:00:01 -0300

    Atualização para GitHub

--- TESTANDO_CODIGO/name_function.py
+++ TESTANDO_CODIGO/name_function.py
@@ -1,6 +1,6 @@
-#
+# PROGRAMA SIMPLES QUE MOSTRA UM NOME E SOBRENOME DE FORMA ELEGANTE
 
-def get_formated_name(first,last):
+def get_formatted_name(first,last):
     """Gera um nome completo formatado de forma elegante"""
     full_name = first + ' ' + last
     return full_name.title()
