ce1bac37254f6a99ea5812533b35bbed25d630d3
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-28 20:00:01 -0300

    Atualização para GitHub

---
+++ ALLIEN_INVASION/0005_alien_invasion_modulo_game_function.py
@@ -0,0 +1,27 @@
+# UTILIZANDO CLASS settings 
+
+# import sys                        # nao é necessario importar sys que é usado em game_functions.py
+import pygame
+from settings import Settings                          
+from ship import Ship                                      
+import game_functions as gf                                 # importando modulo game_function
+
+def run_game():
+    # Inicializa o jogo e cria um objeto para a tela 
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))     
+    pygame.display.set_caption("Alien Invasion")
+
+    # Cria uma espaçonave
+    ship = Ship(screen)
+
+    # Inicia o laço principal do jogo
+    while True:
+        # Observa eventos de teclado e de mouse
+        gf.check_events()
+
+        # chamando update_screen()
+        gf.update_screen(ai_settings,screen,ship)
+
+run_game()
---
+++ ALLIEN_INVASION/game_functions.py
@@ -0,0 +1,18 @@
+import sys
+import pygame
+
+
+def check_events():
+    """Responde a eventos de acionamento de teclas e mouse"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        
+def update_screen(ai_settings,screen,ship):
+    """Atualiza as imagens na tela e alterna para a nova tela"""
+    # Redesenha a tela a cada passagem pelo laço     
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Deixa a tela mais recente visivel
+    pygame.display.flip()
