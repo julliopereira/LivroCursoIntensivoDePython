f4fa110e8fba410110bf69b2aa8042a0476080b7
Julio Cesar Pereira <julliopereira@gmail.com>
2022-09-28 21:30:01 -0300

    Atualização para GitHub

--- ALLIEN_INVASION/EXERCICIO/img.py
+++ ALLIEN_INVASION/EXERCICIO/img.py
@@ -8,4 +8,8 @@ class Img():
         self.rect = self.image.get_rect()
         self.screen_rect = self.screen.get_rect()
 
-        
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def desenhe(self):
+        self.screen.blit(self.image,self.rect)
