d34b7c805dbb053488b2d828d01e513c0abfb6b3
Julio Cesar Pereira <julliopereira@gmail.com>
2022-10-01 14:00:02 -0300

    Atualização para GitHub

--- ALLIEN_INVASION/ship.py
+++ ALLIEN_INVASION/ship.py
@@ -1,9 +1,10 @@
 import pygame
 
 class Ship():
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         """Inicializa a espaçonave e define sua posição inicial"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #Carrega a imagem da espaçonave e obtem seu rect
         self.image = pygame.image.load('images/ship.jpg')
@@ -14,6 +15,9 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        #Armazena um valor decimal para o centro da espaçonave
+        self.center = float(self.rect.centerx)
+
         #Flag de movimento
         self.moving_right = False
         self.moving_left = False
@@ -21,9 +25,11 @@ class Ship():
     def update(self):
         """Atualiza a posição da espaçonave de acordo com a flag de movimento"""
         if self.moving_right:
-            self.rect.centerx += 1 
+            self.center += self.ai_settings.ship_speed_factor
         elif self.moving_left:
-            self.rect.centerx -= 1
+            self.center -= self.ai_settings.ship_speed_factor
+        #Atualiza o objeto rect de acordo com self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Desenha a espaçonave em sua posição atual"""
