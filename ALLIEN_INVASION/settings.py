fc715bc49b43c6e6224d59ad98a62ba73ac5ad2a
Julio Cesar Pereira <julliopereira@gmail.com>
2022-10-01 13:30:01 -0300

    Atualização para GitHub

--- ALLIEN_INVASION/settings.py
+++ ALLIEN_INVASION/settings.py
@@ -7,5 +7,7 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (255, 255, 255)
+        # Configuração da espeçonave
+        self.ship_speed_factor = 1.5
 
     
