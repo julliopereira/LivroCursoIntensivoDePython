4915b433a162813ed43a0b5ceca51ade4215516e
Julio Cesar Pereira <julliopereira@gmail.com>
2022-10-12 23:00:01 -0300

    Atualização para GitHub

--- ALLIEN_INVASION/game_functions.py
+++ ALLIEN_INVASION/game_functions.py
@@ -37,7 +37,6 @@ def check_events(ai_settings,screen,ship,bullets):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
-        
 def update_screen(ai_settings,screen,ship,bullets):
     """Atualiza as imagens na tela e alterna para a nova tela"""
     # Redesenha a tela a cada passagem pelo laço     
