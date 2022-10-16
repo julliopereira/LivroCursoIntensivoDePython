dc8b9672c88f5c34f57cf89f83d68d1089afddb0
Julio Cesar Pereira <julliopereira@gmail.com>
2022-10-16 15:00:01 -0300

    Atualização para GitHub

--- ALLIEN_INVASION/game_functions.py
+++ ALLIEN_INVASION/game_functions.py
@@ -2,6 +2,15 @@ import sys
 import pygame
 from bullet import Bullet
 
+def check_events(ai_settings,screen,ship,bullets):
+    """Responde a eventos de acionamento de teclas e mouse"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event,ai_settings,screen,ship,bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event,ship)
 
 def check_keydown_events(event,ai_settings,screen,ship,bullets):
     """Responde a pressionamento de tecla"""
@@ -14,6 +23,12 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
     elif event.key == pygame.K_q:                       # USANDO A TECLA q PARA SAIR DO JOGO
         sys.exit()
         
+def check_keyup_events(event,ship):
+    """Responde a soltura de tecla"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 def fire_bullets(ai_settings,screen,ship,bullets):
     """Dispara um projétil se o limite ainda não foi alcançado"""
@@ -22,22 +37,14 @@ def fire_bullets(ai_settings,screen,ship,bullets):
             new_bullet = Bullet(ai_settings,screen,ship)
             bullets.add(new_bullet)
 
-def check_keyup_events(event,ship):
-    """Responde a soltura de tecla"""
-    if event.key == pygame.K_RIGHT:
-        ship.moving_right = False
-    elif event.key == pygame.K_LEFT:
-        ship.moving_left = False
-
-def check_events(ai_settings,screen,ship,bullets):
-    """Responde a eventos de acionamento de teclas e mouse"""
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event,ai_settings,screen,ship,bullets)
-        elif event.type == pygame.KEYUP:
-            check_keyup_events(event,ship)
+def update_bullets(bullets):
+    """Atualiza a posição dos projéteis e se livra dos projéteis antigos."""
+    # Atualiza as posições dos projéteis
+    bullets.update()
+    # Livra-se dos projéteis que desapareceram
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
 
 def update_screen(ai_settings,screen,ship,bullets):
     """Atualiza as imagens na tela e alterna para a nova tela"""
@@ -50,12 +57,3 @@ def update_screen(ai_settings,screen,ship,bullets):
     ship.blitme()
     # Deixa a tela mais recente visivel
     pygame.display.flip()
-
-def update_bullets(bullets):
-    """Atualiza a posição dos projéteis e se livra dos projéteis antigos."""
-    # Atualiza as posições dos projéteis
-    bullets.update()
-    # Livra-se dos projéteis que desapareceram
-    for bullet in bullets.copy():
-        if bullet.rect.bottom <= 0:
-            bullets.remove(bullet)
