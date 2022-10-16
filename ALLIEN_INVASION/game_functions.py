8eba1ffaf9d2cd945fb3b57afaef18b72b70d6fb
Julio Cesar Pereira <julliopereira@gmail.com>
2022-10-16 14:00:01 -0300

    Atualização para GitHub

--- ALLIEN_INVASION/game_functions.py
+++ ALLIEN_INVASION/game_functions.py
@@ -11,6 +11,8 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullets(ai_settings,screen,ship,bullets)
+    elif event.key == pygame.K_q:                       # USANDO A TECLA q PARA SAIR DO JOGO
+        sys.exit()
         
 
 def fire_bullets(ai_settings,screen,ship,bullets):
